9da4dac6ea110e9b64166e9dd0518f3d9980db8a
Scott. <59844307+Nykatas@users.noreply.github.com>
2025-04-07 19:36:46 +0200

    chore: Support of auto and system choice.
    Added support for automatic voice system choosing and a manual setup in the config.lua. Just a QOL feature.

--- config.lua
+++ config.lua
@@ -1,23 +1,34 @@
 Config = {}
 
+Config.Debug = false -- Don't activate this if you're in a live environment.
 Config.MicOffText = "off"
 Config.MeterText  = "m"
 
--- Detection of running Voice System
-local voiceSystemYaca = GetResourceState('yaca-voice') == 'started'
-if voiceSystemYaca then
-print("YACA: " .. GetResourceState('yaca-voice'))
-Config.VoiceSystem = "yaca-voice"
-end
+-- Normally, there is no need to change this. If you are having issues with the automatic detections of your voice system set this to the voice system you're using.
+-- Default: auto
+-- Options: auto, saltychat, yaca-voice, pma-voice
+Config.VoiceSystem = "auto"
 
-local voiceSystemSaltychat = GetResourceState('saltychat') == 'started'
-if voiceSystemSaltychat then
-print("Saltychat: " .. GetResourceState('saltychat'))
-Config.VoiceSystem = "saltychat"
-end
-
-local voiceSystemPMA = GetResourceState('pma-voice') == 'started'
-if voiceSystemPMA then
-print("PMA: " .. GetResourceState('pma-voice'))
-Config.VoiceSystem = "pma-voice"
+if Coifng.VoiceSystem == "auto" then
+    -- Detection of running Voice System
+    local voiceSystemYaca = GetResourceState('yaca-voice') == 'started'
+    
+    if voiceSystemYaca then
+        if Config.Debug then print("YACA: " .. GetResourceState('yaca-voice')) end
+        Config.VoiceSystem = "yaca-voice"
+    end
+    
+    local voiceSystemSaltychat = GetResourceState('saltychat') == 'started'
+    
+    if voiceSystemSaltychat then
+        if Config.Debug then print("Saltychat: " .. GetResourceState('saltychat')) end
+        Config.VoiceSystem = "saltychat"
+    end
+    
+    local voiceSystemPMA = GetResourceState('pma-voice') == 'started'
+    
+    if voiceSystemPMA then
+        if Config.Debug then print("PMA: " .. GetResourceState('pma-voice')) end
+        Config.VoiceSystem = "pma-voice"
+    end
 end
